84ca0882542edc6bad8a010d2fead49bf717e407
Allysson <allyssoncarlos.ac21@gmail.com>
2025-05-11 03:27:32 -0300

    acresentando mais exercicios

--- bacon_com_ovos.py
+++ bacon_com_ovos.py
@@ -25,3 +25,75 @@ def bacon_com_ovos(n):
         return 'Ovos'
     
     return 'Passa fomer'
+
+"""
+Exercicio que eu que fiz sozinho
+
+Crie uma função que simule o processo de entrada e saída de um carro em um estacionamento de um comércio.
+
+Regras:
+
+O valor recebido pela função deve ser do tipo int ou float. Caso contrário, deve gerar um erro.
+
+Para sair do estacionamento, o cliente deve pagar R$ 5,00.
+
+Após o pagamento correto, a função deve permitir a saída do carro, simulando a abertura da cancela.
+
+"""
+
+# def carro_no_estacionamento(c) -> int:
+    # Verificando ser é int ou float
+    # assert isinstance(c, (int, float)), ' O carro dever ser int ou float'
+    
+    # Verificando ser pagamento é maior ou igual a 5
+    # if c >= 5:
+    #     return "Pagamento efetuado com sucesso!"
+
+    # Verificando valor 1 = Não pagou, valor 2 = A cancela levanta 1 e 2 sao código de pagamento que eu que criei
+    # if c == 2:
+    #     return 'Cancela de sair levantada com sucesso!'
+    
+    # Ser o pagamento for menor doque o valor não é pago e nem pode sair do estacionamento
+    # return "Voce tem que pagar o estacionamento!"
+
+# Funçao para efetuar o pagamento -> chat
+
+def efetuar_pagamento(valor) -> str:
+    assert isinstance(valor, (int, float)), 'o valor deve ser int ou float'
+
+    if valor >= 5:
+        return "Pagamento efetuado com sucesso!"
+    return "Voce tem que pagar o estacionamento!"
+
+def sair_do_estacionamento(pagamento:bool) -> str:
+    
+    if pagamento:
+        return "Cancela de saida levantada com sucesso!"
+    return "Pagamento necessário antes de sair."
+
+
+
+
+# ✅ Regras:
+# A nota deve ser obrigatoriamente do tipo int ou float. ok
+
+# Se a nota for maior ou igual a 7.0, o aluno está Aprovado. ok
+
+# Se a nota for menor que 7.0, o aluno está Reprovado. ok
+
+# Se a nota for de tipo inválido (como string, lista, etc), deve lançar um erro. ok
+
+# Se a nota for menor que 0 ou maior que 10, deve mostrar "Nota inválida".
+
+def nota_aluno(nota: int | float):
+    assert isinstance(nota, (int, float)), 'A nota do Aluno dever ser int ou float'
+    
+    if nota < 0 or nota > 10:
+        return "Nota inválida"
+
+    if nota >= 7.0:
+        return 'o aluno está Aprovado.'
+    
+    return 'o aluno está Reprovado.'
+
+print(nota_aluno(7))
--- test_baconcomovos.py
+++ test_baconcomovos.py
@@ -15,56 +15,104 @@ Parte 3 -> Melhorar meu código
 import unittest
 from bacon_com_ovos import *
 
-class TestBaconComOvos(unittest.TestCase):
-    # 1 -> Teste ele retorna o erro da função AssertionError
-    def test_bacon_com_ovos_deve_levantar_assertion_error_se_nao_receber_int(self):
+# class TestBaconComOvos(unittest.TestCase):
+#     # 1 -> Teste ele retorna o erro da função AssertionError
+#     def test_bacon_com_ovos_deve_levantar_assertion_error_se_nao_receber_int(self):
+#         with self.assertRaises(AssertionError):
+#             bacon_com_ovos('')
+
+#     #2 -> Teste ser o numero é multiplo de 3 e 5
+#     # def test_bacon(self):    -> Testei esse código so para sab sobre um teste só
+#     #     self.assertEqual(bacon_com_ovos(15), 'Bacon com ovos')
+#     def test_bacon_com_ovos_deve_retorna_se_entrada_for_multiplo_de_3_e_5(self):
+#         entradas = (15, 30)
+#         saida = "Bacon com ovos"
+
+#         for entrada in entradas:
+#             with self.subTest(entrada=entrada, saida=saida):
+#                 self.assertEqual(
+#                     bacon_com_ovos(entrada), saida, msg=f'"{entrada}" não retorna "{saida}"'
+#                 )
+
+#     #3 -> Teste ser não é multiplos de 3 e 5
+#     def test_bacon_com_ovos_deve_retorna_passa_fomer_se_entrada_nao_for_multiplo_de_3_e_5(self):
+#         entradas = (1, 2, 4, 7, 8)
+#         saida = "Passa fomer"
+
+#         for entrada in entradas:
+#             with self.subTest(entrada=entrada, saida=saida):
+#                 self.assertEqual(
+#                     bacon_com_ovos(entrada), saida, msg=f'"{entrada}" não retorna "{saida}"'
+#                 )
+    
+#     #4 -> Test ser a entrada for multiplo de 3
+#     def test_bacon_com_ovos_deve_retorna_Bacon_se_entrada_for_multiplo_de_3(self):
+#         entradas = (3, 6, 9, 12, 18, 21)
+#         saida = "Bacon"
+
+#         for entrada in entradas:
+#             with self.subTest(entrada=entrada, saida=saida):
+#                 self.assertEqual(
+#                     bacon_com_ovos(entrada), saida, msg=f'"{entrada}" não retorna "{saida}"'
+#                 )
+    
+#     #5 -> Test ser a entrada for multiplo de 5
+#     def test_bacon_com_ovos_deve_retorna_Bacon_se_entrada_for_multiplo_de_5(self):
+#         entradas = (50, 55, 65, 70)
+#         saida = "Ovos"
+
+#         for entrada in entradas:
+#             with self.subTest(entrada=entrada, saida=saida):
+#                 self.assertEqual(
+#                     bacon_com_ovos(entrada), saida, msg=f'"{entrada}" não retorna "{saida}"'
+#                 )
+
+
+# class TestCarro(unittest.TestCase):
+    # Verificando ser o c é int ou float
+    # def test_verificando_ser_o_carro_e_int_ou_float(self):
+    #     with self.assertRaises(AssertionError):
+    #         carro_no_estacionamento('')
+
+    # # Verificando ser o pagamento é no valor certo...
+    # def test_pagemento_do_estacionamento_valor_5(self):
+    #     self.assertEqual(carro_no_estacionamento(5), 'Pagamento efetuado com sucesso!')
+
+    # # Verificando quando o pagamento for pago ele pode sair (Valor 1 = Não pagou, Valor 2 = Cancela levantada)
+    # def test_saindo_do_estacionamento_depois_que_efetuou_pagamento_5(self):
+    #     self.assertEqual(carro_no_estacionamento(2), 'Cancela de sair levantada com sucesso!')
+
+# class TestEstacionamento(unittest.TestCase):
+
+#     def test_pagamento_tipo_invalido(self):
+#         with self.assertRaises(AssertionError):
+#             efetuar_pagamento('fd')
+    
+#     def test_pagemento_com_valor_insuficiente(self):
+#         self.assertEqual(efetuar_pagamento(3), "Voce tem que pagar o estacionamento!")
+
+#     def test_pagamento_com_valor_exato(self):
+#         self.assertEqual(efetuar_pagamento(5), "Pagamento efetuado com sucesso!")
+
+#     def test_saida_do_pagamento(self):
+#         self.assertEqual(sair_do_estacionamento(True), "Cancela de saida levantada com sucesso!")
+
+#     def test_saida_do_pagamento_nagada(self):
+#         self.assertEqual(sair_do_estacionamento(False), "Pagamento necessário antes de sair.")
+
+
+class Test_nota_aluno(unittest.TestCase):
+    def test_ser_a_nota_e_int_ou_float(self):
         with self.assertRaises(AssertionError):
-            bacon_com_ovos('')
-
-    #2 -> Teste ser o numero é multiplo de 3 e 5
-    # def test_bacon(self):    -> Testei esse código so para sab sobre um teste só
-    #     self.assertEqual(bacon_com_ovos(15), 'Bacon com ovos')
-    def test_bacon_com_ovos_deve_retorna_se_entrada_for_multiplo_de_3_e_5(self):
-        entradas = (15, 30)
-        saida = "Bacon com ovos"
-
-        for entrada in entradas:
-            with self.subTest(entrada=entrada, saida=saida):
-                self.assertEqual(
-                    bacon_com_ovos(entrada), saida, msg=f'"{entrada}" não retorna "{saida}"'
-                )
-
-    #3 -> Teste ser não é multiplos de 3 e 5
-    def test_bacon_com_ovos_deve_retorna_passa_fomer_se_entrada_nao_for_multiplo_de_3_e_5(self):
-        entradas = (1, 2, 4, 7, 8)
-        saida = "Passa fomer"
-
-        for entrada in entradas:
-            with self.subTest(entrada=entrada, saida=saida):
-                self.assertEqual(
-                    bacon_com_ovos(entrada), saida, msg=f'"{entrada}" não retorna "{saida}"'
-                )
+            nota_aluno('')
     
-    #4 -> Test ser a entrada for multiplo de 3
-    def test_bacon_com_ovos_deve_retorna_Bacon_se_entrada_for_multiplo_de_3(self):
-        entradas = (3, 6, 9, 12, 18, 21)
-        saida = "Bacon"
+    def test_ser_a_nota_e_maio_que_7(self):
+        self.assertEqual(nota_aluno(7), 'o aluno está Aprovado.')
 
-        for entrada in entradas:
-            with self.subTest(entrada=entrada, saida=saida):
-                self.assertEqual(
-                    bacon_com_ovos(entrada), saida, msg=f'"{entrada}" não retorna "{saida}"'
-                )
-    
-    #5 -> Test ser a entrada for multiplo de 5
-    def test_bacon_com_ovos_deve_retorna_Bacon_se_entrada_for_multiplo_de_5(self):
-        entradas = (50, 55, 65, 70)
-        saida = "Ovos"
+    def test_ser_a_nota_e_valida(self):
+        self.assertEqual(nota_aluno(-1), "Nota inválida")
 
-        for entrada in entradas:
-            with self.subTest(entrada=entrada, saida=saida):
-                self.assertEqual(
-                    bacon_com_ovos(entrada), saida, msg=f'"{entrada}" não retorna "{saida}"'
-                )
+    def test_ser_a_nota_e_menor_que_a_media_7(self):
+        self.assertEqual(nota_aluno(4), 'o aluno está Reprovado.')
 
 unittest.main(verbosity=2)
